7c3955500407c0bfa82a0dc017aa2a01db119da0
Lyqst <benja.appel@gmail.com>
2021-09-15 11:32:28 -0300

    rewrite hover funcs, add hover anim, change .brand

--- src/NavItem.svelte
+++ src/NavItem.svelte
@@ -2,29 +2,33 @@
     export let brand = false;
     let hover = false;
 
-    let color = "";
+    let color = 1;
 
-    let list = [
-        "rgb(217, 3, 104)",
-        "rgb(34, 116, 165)",
-        "rgb(247, 92, 3)",
-        "rgb(241, 196, 15)",
-        "rgb(145, 245, 173)",
-    ];
-
-    function toggleHover() {
-        hover = !hover;
-        color = list[Math.floor(Math.random() * list.length)];
+    function toggleHoverIn() {
+        if (!brand) {
+            hover = true;
+            color = Math.floor(Math.random() * 6 + 1);
+        }
     }
+
+    function toggleHoverOut() {
+        hover = false;
+    }
+
 </script>
 
 <div class="navitem-container">
     <div
         class:hover
         class:brand
-        on:mouseenter={toggleHover}
-        on:mouseleave={toggleHover}
-        style="--bg-color:{color}"
+        class:color1="{hover && color == 1}"
+        class:color2="{hover && color == 2}"
+        class:color3="{hover && color == 3}"
+        class:color4="{hover && color == 4}"
+        class:color5="{hover && color == 5}"
+        class:color6="{hover && color == 6}"
+        on:mouseenter={toggleHoverIn}
+        on:mouseleave={toggleHoverOut}
     >
         <a href="/"><slot /></a>
     </div>
@@ -33,8 +37,7 @@
 <style>
     div {
         background-color: rgba(255, 255, 255, 0);
-        transition: ease-in-out;
-        transition-duration: 0.15s;
+        transition: 0.15s ease;
         border-radius: 50%;
         height: 214px;
         width: 214px;
@@ -43,11 +46,12 @@
     }
 
     a {
-        color: black;
+        color: lavender;
         font-size: 24px;
         width: inherit;
         text-decoration: none;
         text-align: center;
+        transition: 0.15s ease;
     }
 
     .navitem-container {
@@ -58,20 +62,58 @@
     }
 
     .hover {
-        transform: scale(1.2);
-        background-color: var(--bg-color);
+        animation: hoverAnim 4s ease-in-out 0.15s infinite;
         z-index: 2;
     }
+
     .hover a {
         color: lavender;
+        transform: scale(1.3);
+    }
+
+    @keyframes hoverAnim {
+        0% {
+            transform: scale(1);
+        }
+        50% {
+            transform: scale(1.2);
+        }
+        100% {
+            transform: scale(1);
+        }
     }
 
     .brand {
-        transform: scale(1);
-        background-color: rgba(13, 255, 174, 255);
+        background: lavender;
     }
 
     .brand a {
+        animation: brandBg infinite 20s cubic-bezier(1, 0, 0.35, 0.9);
         font-size: 38px;
     }
+    
+    @keyframes brandBg {
+        0% {
+            color: var(--color1);
+        }
+        16% {
+            color: var(--color2);
+        }
+        33% {
+            color: var(--color3);
+        }
+        50% {
+            color: var(--color4);
+        }
+        66% {
+            color: var(--color5);
+        }
+        83% {
+            color: var(--color6);
+        }
+        100% {
+            color: var(--color1);
+        }
+    }
+
 </style>
