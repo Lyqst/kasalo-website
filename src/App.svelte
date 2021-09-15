7fd78759771ac9458e94fae37c2d226962d24c82
Lyqst <benja.appel@gmail.com>
2021-09-15 11:31:03 -0300

    add translucent background for nav

--- src/App.svelte
+++ src/App.svelte
@@ -8,32 +8,39 @@
 <main>
 	<Fullpage>
 		<FullpageSection center>
-			<Grid container>
-				<Grid xs={0} sm={1} md={2} lg={3} />
-				<Grid xs={12} sm={10} md={8} lg={6}>
-					<Grid container>
-						<Grid xs={6} sm={6} md={2}>
-							<NavItem>Arte</NavItem>
-						</Grid>
-						<Grid xs={6} sm={6} md={2}>
-							<NavItem>Psicologia</NavItem>
-						</Grid>
-						<Grid xs={12} sm={12} md={4}>
-							<NavItem brand>KASALO</NavItem>
-						</Grid>
-						<Grid xs={6} sm={6} md={2}>
-							<NavItem>Talleres</NavItem>
-						</Grid>
-						<Grid xs={6} sm={6} md={2}>
-							<NavItem>Contacto</NavItem>
+			<div class="nav">
+				<Grid container>
+					<Grid xs={0} sm={1} md={2} lg={3} />
+					<Grid xs={12} sm={10} md={8} lg={6}>
+						<Grid container>
+							<Grid xs={6} sm={6} md={2}>
+								<NavItem>Arte</NavItem>
+							</Grid>
+							<Grid xs={6} sm={6} md={2}>
+								<NavItem>Psicologia</NavItem>
+							</Grid>
+							<Grid xs={12} sm={12} md={4}>
+								<NavItem brand>KASALO</NavItem>
+							</Grid>
+							<Grid xs={6} sm={6} md={2}>
+								<NavItem>Talleres</NavItem>
+							</Grid>
+							<Grid xs={6} sm={6} md={2}>
+								<NavItem>Contacto</NavItem>
+							</Grid>
 						</Grid>
 					</Grid>
+					<Grid xs={0} sm={1} md={2} lg={3} />
 				</Grid>
-				<Grid xs={0} sm={1} md={2} lf={3} />
-			</Grid>
+			</div>
 		</FullpageSection>
 	</Fullpage>
 </main>
 
 <style>
+	.nav {
+		width: 100%;
+		background-color: rgba(0, 0, 0, 0.1);
+		padding: 15px 0px;
+	}
 </style>
