eb3c9755eb617d5fb416b99c181964911b0feca3
Lyqst <benja.appel@gmail.com>
2021-09-17 19:25:14 -0300

    change font to Alata

--- src/App.svelte
+++ src/App.svelte
@@ -6,9 +6,12 @@
 </script>
 
 <svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500&display=swap" rel="stylesheet"> 
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Alata&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
 
 <main>
